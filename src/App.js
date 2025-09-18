98b980aed9591baeef979db02e3e3ce21678c64f
Kavin-crew <abreganakavin@gmail.com>
2025-09-18 15:09:26 +0800

    init files

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import UgcGallery from "./components/UgcGallery";
 function App() {
   //Instance ID
   let InstanceId_Widget = "1211271";
-  let InstanceId_QA = "";
+  let InstanceId_QA = "1214381";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "1213201";
